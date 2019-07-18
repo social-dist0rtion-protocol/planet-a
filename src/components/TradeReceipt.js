51f2fa1cdd9b7fbc0602eb9da4a75a2adccc9b56
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-07-18 15:12:37 +0200

    Add heavy gif and cpu-heavy confetti animation 😈

--- src/components/TradeReceipt.js
+++ src/components/TradeReceipt.js
@@ -3,6 +3,8 @@ import React, { Component } from "react";
 import { Flex, Box } from "rimble-ui";
 import styled from "styled-components";
 import { Blockie } from "dapparatus";
+import handshake from "../assets/handshake.gif";
+import Confetti from "react-dom-confetti";
 
 const Hero = styled.div`
   margin: auto;
@@ -14,6 +16,11 @@ const Hero = styled.div`
   font-size: ${props => props.size * 11 + "vw"};
   border-radius: 50%;
   line-height: 1.05;
+  background-image: url(${handshake});
+  background-repeat:no-repeat;
+  background-position: ${props => props.size * -4.5 + "vw"};
+  background-size: ${props => props.size * 28 + "vw"};
+  transform: scaleX(${props => props.orientation});
   &::before, &::after {
     float: left;
     position: absolute;
@@ -24,26 +31,61 @@ const Hero = styled.div`
 `;
 
 const blockieSize = 10;
+const config = {
+  angle: 90,
+  spread: 90,
+  startVelocity: 45,
+  elementCount: "200",
+  dragFriction: 0.1,
+  duration: 6000,
+  stagger: 0,
+  width: "10px",
+  height: "10px",
+  colors: ["#a864fd", "#29cdff", "#78ff44", "#ff718d", "#fdff6a"]
+};
 
 export default class TradeReceipt extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      orientation: Math.round(Math.random()) === 0 ? 1 : -1,
+      explosion: false
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ explosion: true });
+  }
+
   render() {
     const {
       receipt: { from, to, profit, emission }
     } = this.props;
+    const { orientation, explosion } = this.state;
+
     return (
-      <Flex alignItems="center" justifyContent="space-between">
-        <Box style={{ textAlign: "center" }} width={1 / 5}>
-          <Blockie address={from} config={{ size: blockieSize }} />
-        </Box>
-        <Box style={{ textAlign: "center" }} width={3 / 5}>
-          <Hero size={1.2} before={`+ $${profit}`} after={`+ ${emission}t CO2`}>
-            🤝
-          </Hero>
-        </Box>
-        <Box style={{ textAlign: "center" }} width={1 / 5}>
-          <Blockie address={to} config={{ size: blockieSize }} />
-        </Box>
-      </Flex>
+      <div>
+        <Flex alignItems="center" justifyContent="space-between">
+          <Box style={{ textAlign: "center" }} width={1 / 5}>
+            <Blockie address={from} config={{ size: blockieSize }} />
+          </Box>
+          <Box style={{ textAlign: "center" }} width={3 / 5}>
+            <Hero
+              orientation={orientation}
+              size={1.5}
+              before={`+ $${profit}`}
+              after={`+ ${emission}t CO2`}
+            />
+          </Box>
+          <Box style={{ textAlign: "center" }} width={1 / 5}>
+            <Blockie address={to} config={{ size: blockieSize }} />
+          </Box>
+        </Flex>
+        <Flex alignItems="center" justifyContent="center">
+          <Confetti active={explosion} config={config} />
+        </Flex>
+      </div>
     );
   }
 }
