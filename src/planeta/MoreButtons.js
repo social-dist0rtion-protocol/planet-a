6c8a5ab3e133278702bcfe10e31ff36cc2cc95fb
Max Daunarovich <max.daunarovich@gmail.com>
2019-08-17 02:00:37 +0300

    Refactor MoreButtons to use Rimble and remove unnecessary wrappers

--- src/planeta/MoreButtons.js
+++ src/planeta/MoreButtons.js
@@ -3,69 +3,71 @@ import React from "react";
 import { Flex, Icon, Box } from "rimble-ui";
 import { PrimaryButton } from "../components/Buttons";
 
-export default ({ changeView, defaultPassport, changeAlert }) => {
+export default ({ changeView, passport, changeAlert }) => {
   const passportAlert = () =>
     changeAlert({ type: "warning", message: "Select a passport" });
   return (
-    <div>
-      <Flex mx={-2}>
-        <Box flex={1} m={2}>
-          <PrimaryButton
-            fullWidth
-            onClick={() => {
-              if (defaultPassport) {
-                changeView("planet_a_handshake");
-              } else {
-                passportAlert();
-              }
-            }}
-          >
-            <Flex alignItems="center">
-              <Icon name="Loop" mr={2} />
-              Handshake
-            </Flex>
-          </PrimaryButton>
-        </Box>
-        <Box flex={1} m={2}>
-          <PrimaryButton
-            fullWidth
-            onClick={() => {
-              changeAlert({ type: "warning", message: "Not yet implemented" });
-              /*
-            if (defaultPassport) {
-              changeView("planet_a_plant_trees");
+    <Flex
+      pb={3}
+      mb={3}
+      borderBottom={"1px solid #DFDFDF"}
+      width="100%"
+      flexDirection="column"
+    >
+      <Flex mb={3}>
+        <PrimaryButton
+          mr={2}
+          fullWidth
+          onClick={() => {
+            if (passport) {
+              changeView("planet_a_handshake");
             } else {
               passportAlert();
             }
-            */
-            }}
-          >
-            <Flex mx={-2} alignItems="center">
-              <Icon name="NaturePeople" mr={2} />
-              Plant Trees
-            </Flex>
-          </PrimaryButton>
-        </Box>
+          }}
+        >
+          <Flex alignItems="center">
+            <Icon name="Loop" mr={2} />
+            Handshake
+          </Flex>
+        </PrimaryButton>
+        <PrimaryButton
+          ml={2}
+          fullWidth
+          onClick={() => {
+            changeAlert({ type: "warning", message: "Not yet implemented" });
+            /*
+          if (passport) {
+            changeView("planet_a_plant_trees");
+          } else {
+            passportAlert();
+          }
+          */
+          }}
+        >
+          <Flex mx={-2} alignItems="center">
+            <Icon name="NaturePeople" mr={2} />
+            Plant Trees
+          </Flex>
+        </PrimaryButton>
       </Flex>
-      <Flex mx={-2}>
-        <Box flex={1} m={2}>
-          <PrimaryButton
-            fullWidth
-            onClick={() => {
-              if (defaultPassport) {
-                changeView("planet_a_transfer_passport");
-              } else {
-                passportAlert();
-              }
-            }}
-          >
-            <Flex alignItems="center">
-              <Icon name="Forward" mr={2} />
-              Transfer Passport
-            </Flex>
-          </PrimaryButton>
-        </Box>
+      <Flex>
+        <PrimaryButton
+          fullWidth
+          onClick={() => {
+            if (passport) {
+              changeView("planet_a_transfer_passport");
+            } else {
+              passportAlert();
+            }
+          }}
+        >
+          <Flex alignItems="center">
+            <Icon name="Forward" mr={2} />
+            Transfer Passport
+          </Flex>
+        </PrimaryButton>
       </Flex>
-    </div>
+    </Flex>
   );
 };
