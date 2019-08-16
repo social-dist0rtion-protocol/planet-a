5e67a662b925bb8c86780ba0bbd895c3b5aad2b3
Tim Daubenschütz <tim.daubenschuetz@gmail.com>
2019-08-16 18:27:45 +0200

    Add Error receipts

--- src/components/Receipt.js
+++ src/components/Receipt.js
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 import TxReceipt from "./TxReceipt";
 import TradeReceipt from "./TradeReceipt";
 import PassportReceipt from "./PassportReceipt";
+import ErrorReceipt from "./ErrorReceipt";
 
 export default class Receipt extends Component {
   render() {
@@ -13,10 +14,7 @@ export default class Receipt extends Component {
     if (type === "trade") {
       return <TradeReceipt {...this.props} />;
     } else if (type === "error") {
-      // TODO: Display funny error
-      // NOTE: Alberto told me that Earth can run out of Goellars or CO2 so
-      // we should annoy the shit out of him when an error occurs
-      console.log("display an error");
+      return <ErrorReceipt {...this.props} />;
     } else if (type === "plant") {
       // TODO: Implement tree receipt
       console.log("display a plant");
