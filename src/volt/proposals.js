8c7d1487bb6712455a57af4443d0a4021a0b4ab6
Max Daunarovich <max.daunarovich@gmail.com>
2019-08-29 22:53:31 +0300

    Add hardcoded proposal

---
+++ src/volt/proposals.js
@@ -0,0 +1,13 @@
+const proposals = [
+  {
+    proposalId: "prop-12",
+    title: "bla",
+    description: "bla bla bla",
+    topic: ["Smart State", "Social Equality"],
+    boothAddress: "0x1ba5dca50564df62f60a4930635f7dbbf38378b7",
+    yesBoxAddress: "0xdea3f5922b9bbde4707d1f73d4d265040c49282b",
+    noBoxAddress: "0x772e6c04dcb3b42d716db90699de6664f4ba45ba"
+  }
+];
+
+export default proposals;
