c7e9dbca2ac7f0a46521e69d8abbe73644d8a647
Alex Lunyov <isuntc@gmail.com>
2019-09-05 21:16:19 +0800

    Update src/volt/components/ProposalsList/SingleProposal/styles.js
    Co-Authored-By: Maksim Daunarovich <max.daunarovich@gmail.com>

--- src/volt/components/ProposalsList/SingleProposal/styles.js
+++ src/volt/components/ProposalsList/SingleProposal/styles.js
@@ -44,7 +44,10 @@ export const Topic = styled(Flex).attrs(() => ({
   borderColor: "voltBrandDarkGrey"
 }))`
   border: 1px solid;
-
+margin-right: 5px;
+&:last-of-type{
+  margin-right: 0;
+}
   & + & {
     margin-left: 5px;
   }
